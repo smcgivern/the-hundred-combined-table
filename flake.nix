5ef9e6116a08c79ac2a9cd1552fa3a5bcd96d962
Sean McGivern <sean@mcgivern.me.uk>
2026-07-26 16:47:31 +0100

    Bump to Go 1.26

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     let pkgs = import nixpkgs { inherit system; };
     in {
       devShells.default = pkgs.mkShellNoCC {
-        buildInputs = with pkgs; [ go_1_19 ];
+        buildInputs = with pkgs; [ go_1_26 ];
       };
     }
   );
